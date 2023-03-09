72d9b540cf14814660d0ea27af0b3462157e7f75
B1129011 <119965133+B1129011@users.noreply.github.com>
2023-03-09 13:54:41 +0800

    Create main.c

---
+++ Lab#1/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+    int a[]={1,2,3};
+    char b[]="ABC"
+    float c[]={1.1, 1.2, 1.3 };
+    FILE*fp;
+    if((fp=fopen("a.bin","wb+"))==NULL){
+        printf("Cannot open the file...");
+        exit(1);
+    }
+    if(fwrite(arr_write,sizeof(arr_write),1,fp)!=1){
+        printf("File write erro...\n");
+    }
+    fseek(fp,0,SEEK_SET);
+    
+    if(fread(arr_write,sizeof(arr_write),1,fp)!=1){
+        printf("File read erro...\n");
+    }
+
+    return 0;
+}
