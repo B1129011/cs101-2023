ab74a804d2e19462ba9fa59133ddc446b2ce1447
B1129011 <119965133+B1129011@users.noreply.github.com>
2023-05-10 20:35:30 +0800

    Update main.cpp

--- HW#4/main.cpp
+++ HW#4/main.cpp
@@ -1,114 +1,93 @@
-#include <stdio.h>
-
-#define ROWS 3
-#define COLS 3
-
-char matrix[ROWS][COLS] = {
-    {'1', '2', '3'},
-    {'4', '5', '6'},
-    {'7', '8', '9'}
+#include <iostream>
+#include <limits>
+using namespace std;
+class input{
+    private:
+        int x,y,num;
+        bool player=true;
+        void transfer(){
+            cin>>num;
+            x=(num-1)%3;
+            y=(num-1)/3;
+        }
+    public:
+        char matrix[3][3];
+        void playerInput(){
+            while(true){
+                transfer();
+                if(cin.fail()){
+                    cout<<"請只輸入整數"<<endl;
+                    cin.clear();
+                    cin.ignore(numeric_limits<streamsize>::max(),'\n');
+                    continue;
+                }
+                if(num>9||num<1){
+                    cout<<"請輸入有效坐標 (1-9)"<<endl;
+                    continue;
+                }
+                if(matrix[y][x]){
+                    cout << "這格已經被選走了，請選擇其他格" << endl;
+                    continue;
+                }
+                matrix[y][x]=(player)?'X':'O';
+                player=!player;
+                break;
+            }
+        }
+        void checkerboard_builder(){
+            cout<<"-- Tic Tac Toe -- CSIE@CGU"<<endl;
+            cout<<"player 1 (X) - player 2 (O)"<<endl;
+            for(int i=0;i<3;i++){
+                for(int j=0;j<3;j++){
+                    char str=(matrix[i][j]=='X'||matrix[i][j]=='O')?matrix[i][j]:' ';
+                    cout << str << "|";
+                }
+                cout <<endl;
+            }
+        }
 };
 
-void draw_board();
-int get_input(char symbol);
-char check_win();
-void clear_board();
+class TicTacToe:input{
+    private:
+        int n=0;
+        bool win=false;
+    public:
+        void checkWin(){
+            char symbols[2]={'X','O'};
+                for(char sym:symbols){
+                    for(int i=0;i<3;i++){
+                        if((matrix[i][0]==sym&&matrix[i][1]==sym&&matrix[i][2]==sym)||  // 水平檢查
+                           (matrix[0][i]==sym&&matrix[1][i]==sym&&matrix[2][i]==sym)) {   // 垂直檢查
+                            win=true;
+                            break;
+                        }
+                    }
+                    if((matrix[0][0]==sym&&matrix[1][1]==sym&&matrix[2][2]==sym)||      // 對角線檢查
+                       (matrix[0][2]==sym&&matrix[1][1]==sym&&matrix[2][0]==sym)){
+                        win=true;
+                    }
+                if(win){
+                    cout<<"==>Player "<<(sym=='X'?"1":"2")<<" wins"<< endl;
+                    return;
+                }
+                if(n==8){
+                    cout<<"game draw"<<endl;
+                }
+            }
+        }
+        void run(){
+            while(n<9&&!win){
+                playerInput();
+                checkerboard_builder();
+                checkWin();
+                n++;
+            }
+        }
+};
 
-int main() {
-    printf("-- Tic Tac Toe -- CSIE@GCU\n");
-    printf("Player 1 (X) - Player 2 (O)\n");
-    char symbol = 'X';  
-    char winner = '\0'; 
-
-    do {
-        draw_board();          
-        int position = get_input(symbol);  
-        int row = (position - 1) / 3;
-        int col = (position - 1) % 3;
-        matrix[row][col] = symbol;  
-        winner = check_win();
-        symbol = (symbol == 'X') ? 'O' : 'X';
-    } while (winner == '\0');
-
-    draw_board();
-    printf("==>Player %c wins!\n", winner)
-
-    clear_board(); 
-
+int main()
+{
+    TicTacToe game;
+    game.run();
     return 0;
-}
-
-// 繪製遊戲矩陣
-void draw_board() {
-    printf("\n");
-    printf("     \u25A0     \u25A0  \n");
-    printf("  %c  \u25A0  %c  \u25A0  %c  \n", matrix[0][0], matrix[0][1], matrix[0][2]);
-    printf("     \u25A0     \u25A0  \n");
-    printf("\u25A0\u25A0\u25A0\u25A0\u25A0\u25A0\u25A0\u25A0\u25A0\u25A0\u25A0\u25A0\u25A0\u25A0\u25A0\u25A0\n");
-    printf("     \u25A0     \u25A0  \n");
-    printf("  %c  ■  %c  ■  %c  \n", matrix[1][0], matrix[1][1], matrix[1][2]);
-    printf("     \u25A0     \u25A0  \n");
-    printf("\u25A0\u25A0\u25A0\u25A0\u25A0\u25A0\u25A0\u25A0\u25A0\u25A0\u25A0\u25A0\u25A0\u25A0\u25A0\u25A0\n");
-    printf("     \u25A0     \u25A0  \n");
-    printf("  %c  \u25A0  %c  \u25A0  %c  \n", matrix[2][0], matrix[2][1], matrix[2][2]);
-    printf("     \u25A0     \u25A0  \n");
-    printf("\n");
-}
-
-int get_input(char symbol) {
-    int position = 0;
-    do {
-        printf("Player %c, enter a position (1-9): ", symbol);
-        scanf("%d", &position);
-    } while (position < 1 || position > 9 || matrix[(position-1)/3][(position-1)%3] == 'X' || matrix[(position-1)/3][(position-1)%3] == 'O');
-    return position;
-}
-
-char check_win() {
-    int i, j;
-    char winner = '\0';
-    for (i = 0; i < ROWS; i++) {
-        if (matrix[i][0] == matrix[i][1] && matrix[i][0] == matrix[i][2]) {
-winner = matrix[i][0];
-break;
-}
-}
-if (winner == '\0') {
-for (j = 0; j < COLS; j++) {
-if (matrix[0][j] == matrix[1][j] && matrix[0][j] == matrix[2][j]) {
-winner = matrix[0][j];
-break;
-}
-}
-}
-if (winner == '\0') {
-if (matrix[0][0] == matrix[1][1] && matrix[0][0] == matrix[2][2]) {
-winner = matrix[0][0];
-} else if (matrix[0][2] == matrix[1][1] && matrix[0][2] == matrix[2][0]) {
-winner = matrix[0][2];
-}
-}
-if (winner == '\0') {
-int count = 0;
-for (i = 0; i < ROWS; i++) {
-for (j = 0; j < COLS; j++) {
-if (matrix[i][j] == 'X' || matrix[i][j] == 'O') {
-count++;
-}
-}
-}
-if (count == 9) {
-winner = 'T'; // 'T' 代表 Tie
-}
-}
-return winner;
-}
-
-void clear_board() {
-int i, j;
-for (i = 0; i < ROWS; i++) {
-for (j = 0; j < COLS; j++) {
-matrix[i][j] = (char)('0' + i * 3 + j + 1);
-}
-}
 }
